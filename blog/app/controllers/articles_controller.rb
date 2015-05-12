e75d8f773e7057ac4169047931618c7493626b90
583660496 <583660496@qq.com>
2015-05-12 02:57:42 -0700

    add del and update

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,17 +2,24 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
- 
-    @article.save
-    redirect_to @article
+
+    if @article.save
+      redirect_to @article
+    else 
+      render 'new'
+    end
   end
 
   public
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 
   def show
     @article = Article.find(params[:id])
@@ -22,4 +29,25 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+ 
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
 end
